61df52eff6a1cb932bdd0d8dd1d7e9215e365750
Breno <Breno>
2022-01-06 22:23:31 -0300

    Finalizado

--- js/validar/validaCpf.js
+++ js/validar/validaCpf.js
@@ -7,10 +7,27 @@ export class ValidaCpf{
     let a = this.cpf.value;
     let cpfString = a.replace(/(\D)+/g, '')
     let cpfLimpo = Array.from(cpfString);
-    
-    return this.validaPrimeiroDigito(cpfLimpo) && this.validaSegundoDigito(cpfLimpo);
+
+    if(this.verificaCpf(cpfLimpo)){
+      return this.validaPrimeiroDigito(cpfLimpo) && this.validaSegundoDigito(cpfLimpo);
+    }
+
+    return false;
   }
   
+  // CPF do tipo 111.111.111-11 vai passar no algoritmo, mas não existe.
+  verificaCpf(cpfArray){
+    let digito = cpfArray[0];
+    let cpf = cpfArray.join('');
+    let cpfDigitoRepetido = digito.repeat(11);
+
+    if(cpf === cpfDigitoRepetido){
+      return false;
+    }
+
+    return true;
+  }
+
   // 0*10 + 4*9 + 8*8 + 4*7 + 3*6 + 1*5 + 4*4 + 3*3 + 2*2
   validaPrimeiroDigito(cpfArray){
     let j = 10, acc = 0, result;
